6f56b60d49967f24f6c161434fd96511fced80b3
Seba <sebastiantorresola3@gmail.com>
2024-10-27 16:55:25 -0700

    Move types to their own folder

--- frontend/hooks/queries/useFetchCloseParties.ts
+++ frontend/hooks/queries/useFetchCloseParties.ts
@@ -1,5 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import eventMarkers from './mock-data/eventMarkers.json';
+import { Party } from '@/types/Party';
+
+interface PartyData {
+  markers: Party[];
+}
 
 const fetchCloseParties = async (latitude: number, longitude: number, radius: number) => {
   // This is how we will mock data fetching until our backend team is ready
@@ -12,43 +17,3 @@ export const useFetchCloseParties = (latitude: number, longitude: number, radius
     queryFn: () => fetchCloseParties(latitude, longitude, radius),
   });
 };
-
-export interface Ticket {
-  id: number;
-  userId: number;
-  partyId: number;
-  paid: boolean;
-}
-
-export interface Coordinate {
-  latitude: number;
-  longitude: number;
-}
-
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  dateOfBirth: Date;
-  isAdmin: boolean;
-  campus: 'OregonStateUniversity';
-  ownedParties: Party[];
-  tickets: Ticket[];
-}
-
-export interface Party {
-  id: number;
-  type: 'private' | 'public';
-  startDate: string;
-  endDate: string;
-  entryCost: number;
-  title: string;
-  description: string;
-  adminId: number;
-  location: Coordinate;
-  attendees: User[];
-}
-
-export interface PartyData {
-  markers: Party[];
-}

ff135b04918c6da50a7c47cd5557cf8658b6d2de
Seba <sebastiantorresola3@gmail.com>
2024-11-12 17:28:04 -0800

    Create frontend Stripe payment button

--- frontend/app/(tabs)/payment.tsx
+++ frontend/app/(tabs)/payment.tsx
@@ -1,28 +1,66 @@
-import React, { useState } from 'react';
-import { View, Button, Text } from 'react-native';
-import { CardField, useStripe } from '@stripe/stripe-react-native';
+import { useStripe } from '@stripe/stripe-react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Button, Text, Alert } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Payment() {
-    const [cardDetails, setCardDetails] = useState(null);
-    const [paymentStatus, setPaymentStatus] = useState('');
+export default function CheckoutScreen() {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [loading, setLoading] = useState(false);
 
-    const handlePayment = async () => {
-        // Some stripe payment logic
+  const fetchPaymentSheetParams = async () => {
+    const response = await fetch(`http://localhost:3000/payment-sheet`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const { paymentIntent, ephemeralKey, customer } = await response.json();
+
+    return {
+      paymentIntent,
+      ephemeralKey,
+      customer,
     };
+  };
 
-    return (
-        <SafeAreaView>
-            <CardField
-                postalCodeEnabled={true}
-                placeholder={{ number: '4242 4242 4242 4242' }}
-                cardStyle={{ backgroundColor: '#FFFFFF', textColor: '#000000' }}
-                style={{ width: '100%', height: 50, marginVertical: 30 }}
-                onCardChange={(cardDetails) => setCardDetails(cardDetails)}
-            />
-            <Button onPress={handlePayment} title="Pay" />
-            {paymentStatus ? <Text>{paymentStatus}</Text> : null}
-        </SafeAreaView>
-    );
+  const initializePaymentSheet = async () => {
+    const { paymentIntent, ephemeralKey, customer } = await fetchPaymentSheetParams();
+
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: 'Example, Inc.',
+      customerId: customer,
+      customerEphemeralKeySecret: ephemeralKey,
+      paymentIntentClientSecret: paymentIntent,
+      // Set `allowsDelayedPaymentMethods` to true if your business can handle payment
+      //methods that complete payment after a delay, like SEPA Debit and Sofort.
+      allowsDelayedPaymentMethods: true,
+      defaultBillingDetails: {
+        name: 'Jane Doe',
+      },
+    });
+    console.log(error);
+    if (!error) {
+      setLoading(true);
+    }
+  };
+
+  const openPaymentSheet = async () => {
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      Alert.alert('Success', 'Your order is confirmed!');
+    }
+  };
+
+  useEffect(() => {
+    initializePaymentSheet();
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <Button disabled={!loading} title="Checkout" onPress={openPaymentSheet} />
+    </SafeAreaView>
+  );
 }
-
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,19 +2,41 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { useReactQueryDevTools } from '@dev-plugins/react-query';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StripeProvider } from '@stripe/stripe-react-native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 const queryClient = new QueryClient();
 
+interface PaymentSheetResponse {
+  paymentIntent: string;
+  ephemeralKey: string;
+  customer: string;
+  publishableKey: string;
+}
+
 export default function RootLayout() {
+  const [publishableKey, setPublishableKey] = useState('');
+
+  const fetchPublishableKey = async () => {
+    await fetch('http://localhost:3000/payment-sheet')
+      .then((res) => res.json())
+      .then((data: PaymentSheetResponse) => {
+        setPublishableKey(data.publishableKey);
+      });
+  };
+  console.log(publishableKey);
+  useEffect(() => {
+    fetchPublishableKey();
+  }, []);
+
   useReactQueryDevTools(queryClient);
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
@@ -32,13 +54,15 @@ export default function RootLayout() {
   }
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <StripeProvider publishableKey={publishableKey}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </StripeProvider>
   );
 }
